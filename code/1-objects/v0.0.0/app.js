800a371829443bcda4551a52e8ee04161eae68e7
miguelalonsocoba <miguetheking53@gmail.com>
2024-03-16 17:27:42 -0600

    Implementando primeras  clases

--- code/1-objects/v0.0.0/app.js
+++ code/1-objects/v0.0.0/app.js
@@ -7,9 +7,9 @@ function initConnect4View() {
   return {
     play: function () {
       const continueDialogView = initYesNoDialogView("Do you want to play another game?");
-      //   const boardView = initBoardView();
       do {
-        // boardView.start();
+        console.writeln(`\n\n------------ CONNECT4 ------------\n\n`);
+        initBoardView().show();
         continueDialogView.read();
       } while (continueDialogView.isAffirmative());
     },
@@ -39,3 +39,49 @@ function initYesNoDialogView(question) {
     },
   };
 }
+
+function initBoardView() {
+  const board = initBoard();
+
+  function printTurn() {
+    console.writeln(`Turno: Jugador ${board.getPlayer()} - ${board.getColor()}`);
+  }
+
+  function printBoard() {
+    for (let i = 0; i < board.getSquares().length; i++) {
+      for (let j = 0; j < board.getSquares()[0].length; j++) {
+        console.write(` | ${board.getSquares()[i][j]}`);
+      }
+      console.writeln(` | \n -----------------------------`);
+    }
+  }
+
+  return {
+    show: function () {
+      console.writeln(`*****************`);
+      // printTurn();
+      printBoard();
+    },
+  };
+}
+
+function initBoard() {
+  let squares = [
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+    ["_", "_", "_", "_", "_", "_", "_"],
+  ];
+  const player1 = initPlayer();
+  const player2 = initPlayer();
+
+  return {
+    getSquares: function () {
+      return squares;
+    },
+  };
+}
+
+function initPlayer() {}
